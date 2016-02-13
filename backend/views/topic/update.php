07cb1c488df3ad0db30320adecb763c36caf3469
k1785 <k1785@rambler.ru>
2016-02-13 10:10:13 +0300

    add theme admin

--- backend/views/topic/update.php
+++ backend/views/topic/update.php
@@ -1,24 +1,22 @@
 <?php
 
-use yii\helpers\Html;
-
 /* @var $this yii\web\View */
 /* @var $model common\models\Topic */
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => 'Topic',
 ]) . ' ' . $model->title;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Topics'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app/topic', 'Topics'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
-<div class="topic-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-        'tags' => $tags,
-    ]) ?>
-
+<div class="row">
+    <div class="col-md-6">
+        <div class="box box-primary">
+        <?= $this->render('_form', [
+            'model' => $model,
+            'tags' => $tags,
+        ]) ?>
+        </div>
+    </div>
 </div>
